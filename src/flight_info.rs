ba407763b629ac0781d7d7b1d00ec5ce26dcd3f6
unknown <mikeeybikeey@gmail.com>
2023-09-08 14:02:09 -0600

    Add FlightInfo

---
+++ src/flight_info.rs
@@ -0,0 +1,23 @@
+#[derive(Default)]
+pub struct FlightInfo {
+    pub bag_cost: i32,
+    pub bag_count: i32,
+    pub ticket_cost: i32,
+}
+
+impl FlightInfo {
+    pub fn total_cost(&self) -> i32 {
+        self.bag_cost * self.bag_count // + self.ticket_cost * self.passengers.len()
+    }
+}
+
+/// Helper trait to simplify repeated calls to `Cursive::user_data`.
+pub trait GetFlightInfo {
+    fn flight_info(&mut self) -> &mut FlightInfo;
+}
+
+impl GetFlightInfo for cursive::Cursive {
+    fn flight_info(&mut self) -> &mut FlightInfo {
+        self.user_data().unwrap()
+    }
+}
